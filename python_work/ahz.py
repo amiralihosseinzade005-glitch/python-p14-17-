3be086dbb5349ca986fe577ebe1989dfca12ed85
amiralihosseinzade005-glitch <amiralihosseinzade005@gmail.com>
2026-09-17 19:16:06 +0330

    python work

---
+++ python_work/ahz.py
@@ -0,0 +1,66 @@
+print("be kafe resturan az khosh amadid")
+
+factor_resturan=[]
+
+while True:
+     meno =input("1.resturan 2.cafe 3.bar 4.exit:")
+     while True:
+        match meno:
+            case"1":
+                meno_resturan = input("1.kabab800 2.pizza900 3.exit:")
+
+                match meno_resturan:
+
+
+
+                     case"1":
+
+                         num_kabab = int(input("chanta kabab"))
+                         kabab_prise = num_kabab * 800
+                         kabab_prise_tax = kabab_price * 1.1
+                         print("kabab ba maliyat :" ,kabab_price_tax)
+
+
+
+
+                         if kabab_price_tax  >= 5000:
+                             takhfif_kabab = kabab_price_tax * 0.05
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("dargah sazman sanjesh")
+
+vorudi=[]
+
+while True:
+
+     vorudi_meno=("darkhast elam vaziyat")
+
+vaziyat=input("1.sign_in: , 2.sign_up: ")    
+
+
+
+
+
